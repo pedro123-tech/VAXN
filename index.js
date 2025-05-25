f33a361c6020aec529fa95b5ea1ca7cab8d6ac7b
pedro123-tech <pedro111.fernandez.ruz@gmail.comgit>
2025-05-25 22:22:13 +0200

    Añadido archivo HTML de verificación de Search Console

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const upload = multer({ dest: 'uploads/' });
 app.use(express.json());
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 
 
